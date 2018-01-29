0681d8a84c15f14816b965e1f7af70fc20e34fba
Petar Nikolov <penikolov23@gmail.com>
2018-01-29 19:49:16 +0200

    Add a secure password to the User model
    Furthermore, a database index is placed on the user's email field
    to enforce database level uniqueness as well as for faster lookup.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,14 @@
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
+  has_secure_password
+
   validates :name, presence: true, length: { maximum: 50 }
   validates :surname, presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false } 
+  validates :password, presence: true, length: { minimum: 8 }
 
   before_save :downcase_email
 
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,7 +2,8 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   setup do
-    @user = users(:john)
+    @user = User.new(name: 'John', surname: 'Smith', email: 'john@example.com',
+                     password: 'password', password_confirmation: 'password')
   end
 
   test 'should be valid' do
@@ -10,39 +11,39 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test 'name should be present' do
-    @user.name = '  '
-    assert_not @user.valid?, 'Name is blank'
+    @user.name = ''
+    assert @user.invalid?, 'Name is blank'
   end
   
   test 'name should not be longer than 50 characters' do
     @user.name = 'a' * 50
     assert @user.valid?
     @user.name += 'a'
-    assert_not @user.valid?, 'A name longer than 50 letters is accepted'
+    assert @user.invalid?, 'A name longer than 50 letters is accepted'
   end
 
   test 'surname should be present' do
     @user.surname = ''
-    assert_not @user.valid?, 'Surname is blank'
+    assert @user.invalid?, 'Surname is blank'
   end
 
   test 'surname should not be longer than 50 characters' do
     @user.surname = 'a' * 50
     assert @user.valid?
     @user.surname += 'a'
-    assert_not @user.valid?, 'A surname longer than 50 letters is accepted'
+    assert @user.invalid?, 'A surname longer than 50 letters is accepted'
   end
 
   test 'email should be present' do
     @user.email = ''
-    assert_not @user.valid?, 'Email is blank'
+    assert @user.invalid?, 'Email is blank'
   end 
 
   test 'email should not be longer than 255 characters' do
     @user.email = 'a' * 243 + '@example.com'
     assert @user.valid?
     @user.email = 'a' + @user.email
-    assert_not @user.valid?, 'An email longer than 255 letters is accepted'
+    assert @user.invalid?, 'An email longer than 255 letters is accepted'
   end
 
   test 'valid email addresses should be accepted' do
@@ -63,7 +64,7 @@ class UserTest < ActiveSupport::TestCase
     ]
     invalid_emails.each do |email|
       @user.email = email
-      assert_not @user.valid?, "Invalid email (#{email}) address is accepted"
+      assert @user.invalid?, "Invalid email (#{email}) address is accepted"
     end
   end
 
@@ -71,6 +72,25 @@ class UserTest < ActiveSupport::TestCase
     duplicate_user = @user.dup
     @user.save
     assert_not duplicate_user.valid?
+  end
+
+  test 'email address should be saved as lower-case' do
+    mixed_case_email = 'Foo@EXAMPLE.cOm'
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
+
+  test 'password should be present' do
+    @user.password = @user.password_confirmation = ' ' * 8
+    assert @user.invalid?, 'Password is not present'
+  end
+
+  test 'password should be at least 8 characters long' do
+    @user.password = @user.password_confirmation = 'a' * 7
+    assert @user.invalid?, 'Password under 8 characters is accepted'
+    @user.password = @user.password_confirmation = 'a' * 8
+    assert @user.valid?
   end
 end
 
