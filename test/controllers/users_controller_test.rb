e70739d0d4f9a15e6769302d063025f8787d8be6
Petar Nikolov <penikolov23@gmail.com>
2018-01-30 17:43:25 +0200

    Allow users' information to be editted

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -17,6 +17,12 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_template 'users/new'
   end
 
+  test 'should get edit' do
+    get edit_user_path @user
+    assert_response :success
+    assert_template 'users/edit'
+  end
+
   test 'should create a new user when valid information is posted' do
     assert_difference 'User.count', 1 do
       post signup_path, params: { user: { name: 'Albert', surname: 'Einstein',
@@ -35,6 +41,30 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
                                           password_confirmaiton: 'long' } }
     end
     assert_template 'users/new'
+  end
+
+  test 'should not update user when invalid information is posted' do
+    updated_at = @user.updated_at
+    patch user_path(@user), params: { user: { name: '', surname: '',
+                                              email: '',
+                                              password: 'wrong',
+                                              password_confirmation: 'pass' } }
+    @user.reload
+    assert_equal updated_at, @user.updated_at
+  end
+
+  test 'should update user when valid information is posted' do
+    name = 'Albert'
+    surname = 'Einstein'
+    previously_updated = @user.updated_at
+    patch user_path(@user), params: { user: { name: name, surname: surname,
+                                              email: @user.email,
+                                              password:              '',
+                                              password_confirmation: '' } }
+    @user.reload
+    assert_not_equal previously_updated, @user.updated_at
+    assert_equal name, @user.name
+    assert_equal surname, @user.surname
   end
 end
 
---
+++ test/integration/users_edit_test.rb
@@ -0,0 +1,38 @@
+require 'test_helper'
+
+class UsersEditTest < ActionDispatch::IntegrationTest
+  setup do
+    @user = users(:john)
+  end
+
+  test 'unsuccessful edit' do
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    patch user_path(@user), params: { user: { name: '', surname: '',
+                                              email: 'email@invalid',
+                                              password:              'wrong',
+                                              password_confirmation: 'pass' } }
+    assert_template 'users/edit'
+  end
+
+  test 'successful edit' do
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    name = 'Albert'
+    surname = 'Einstein'
+    email = 'genius@example.com'
+    patch user_path(@user), params: { user: { name: name, surname: surname,
+                                              email: email,
+                                              password:              '',
+                                              password_confirmation: '' } }
+    assert_not flash.empty?
+    assert_equal 'Profile updated', flash[:success]
+    assert_response :redirect
+    assert_redirected_to @user
+    @user.reload
+    assert_equal name, @user.name
+    assert_equal surname, @user.surname
+    assert_equal email, @user.email
+  end
+end
+
