7c8c026d8daa987042fb53dfd70dc446d175c7e3
Petar Nikolov <penikolov23@gmail.com>
2018-01-30 12:19:13 +0200

    Implement persistant sessions via cookies

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
+  attr_accessor :remember_token
+
   has_secure_password
 
   validates :name, presence: true, length: { maximum: 50 }
@@ -19,11 +21,32 @@ class User < ApplicationRecord
                                                   BCrypt::Engine.cost
       BCrypt::Password.create(string, cost: cost)
     end
+
+    # Generate a random token
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+
+  # Remember a user in the database for use in persistant sessions
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  # Return true if the given token matches the digest
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  # Forget a user
+  def forget
+    update_attribute(:remember_digest, nil)
   end
 
   private
 
-  # Converts email to all lower-case.
+  # Convert email to all lower-case
   def downcase_email
     email.downcase!
   end
