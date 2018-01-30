d1aded5fded3e7875bbb9fd7903336707431f2d3
Petar Nikolov <penikolov23@gmail.com>
2018-01-30 12:50:57 +0200

    Add 'remember me' checkbox to login view
    Also, add tests for the aforementioned 'remember me' functionallity.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,8 +7,23 @@ class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
+  # Log in as user
+  def log_in_as(user)
+    session[:user_id] = user.id
+  end
+
+  # Return true if the user is logged in
   def logged_in?
     !session[:user_id].nil?
+  end
+end
+
+class ActionDispatch::IntegrationTest
+  # Log in as a user
+  def log_in_as(user, password: 'password', remember_me: '1')
+    post login_path, params: { session: { email: user.email,
+                                          password: password,
+                                          remember_me: remember_me } }
   end
 end
 
