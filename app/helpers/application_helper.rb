437594b760636a6d2ae7065e1afae63cbafd7023
Petar Nikolov <penikolov23@gmail.com>
2018-02-11 19:21:47 +0200

    Move 'breadcrumb_item' to ApplicationHelper
    The helper method 'breadcrumb_item' is utilized in the site's layout as
    a whole and not particularly in the static pages. This is why this
    helper method has been moved from StaticPagesHelper to
    ApplicationHelper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -39,5 +39,17 @@ module ApplicationHelper
     css_classes = [*bootstrap_classes, *extra_classes]
     css_classes.join(' ')
   end
+
+  # Render a hyperlink as a breadcrumb item
+  def breadcrumb_item(text, path)
+    if path == request.env['PATH_INFO']
+      content = text
+      css_classes = 'breadcrumb-item active'
+    else
+      content = link_to text, path
+      css_classes = 'breadcrumb-item'
+    end
+    content_tag(:li, content, class: css_classes)
+  end
 end
 
