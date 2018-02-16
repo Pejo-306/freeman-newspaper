f8ad2b0ad30c33f6817163708cfc3e47fd352604
Petar Nikolov <penikolov23@gmail.com>
2018-02-16 18:11:14 +0200

    Style the admin users index page
    Other changes:
      - restyled bootstrap pagination and badges
      - updated tests to cope with new additions
      - fixed a typo in AdminController

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -4,7 +4,7 @@ class AdminController < ApplicationController
 
   def index
     Rails.application.eager_load! if Rails.env.development?
-    # Retrieve only resources who have been added to the namespace 'admin'
+    # Retrieve only resources which have been added to the namespace 'admin'
     # This is accomplished by evaluating the existence of the
     # appropriate namespaced resource url/path method
     @models = ApplicationRecord.descendants
