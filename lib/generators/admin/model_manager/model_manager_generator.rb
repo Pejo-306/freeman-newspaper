95cbcba351fa9e3f03342479f5333618660a692e
Petar Nikolov <penikolov23@gmail.com>
2018-02-18 20:08:08 +0200

    Implement a complete admin model controller template

--- lib/generators/admin/model_manager/model_manager_generator.rb
+++ lib/generators/admin/model_manager/model_manager_generator.rb
@@ -2,6 +2,12 @@ class Admin::ModelManagerGenerator < Rails::Generators::NamedBase
   source_root File.expand_path('../templates', __FILE__)
 
   def create_model_generator
+    @controller_name = file_name.singularize.capitalize
+    @record_name = @controller_name.downcase
+    model = @controller_name.classify.constantize
+    @model_fields = model.attribute_names - ['id', 'created_at', 'updated_at']
+    @model_fields.map! { |field| ":#{field}" }
+
     template 'controller.rb.erb',
              "app/controllers/admin/#{file_name.pluralize}_controller.rb"
   end
