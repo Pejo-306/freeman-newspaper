02a19a27ddc89cb24986eff6c9a70345943338bc
Petar Nikolov <penikolov23@gmail.com>
2018-02-22 16:01:16 +0200

    Add some setup actions for each generator method
    This commit concerns the admin:model_manager generator: @controller_name
    and @record_name are now available to all of its generator methods.
    Previously they were only present in the method
    'create_model_generator'.

--- lib/generators/admin/model_manager/model_manager_generator.rb
+++ lib/generators/admin/model_manager/model_manager_generator.rb
@@ -1,9 +1,13 @@
 class Admin::ModelManagerGenerator < Rails::Generators::NamedBase
   source_root File.expand_path('../templates', __FILE__)
 
-  def create_model_generator
+  def initialize(*args, **kwargs)
+    super(*args, **kwargs)
     @controller_name = file_name.singularize.capitalize
     @record_name = @controller_name.downcase
+  end
+
+  def create_model_generator
     model = @controller_name.classify.constantize
     @model_fields = model.attribute_names - ['id', 'created_at', 'updated_at']
     @model_fields.map! { |field| ":#{field}" }
