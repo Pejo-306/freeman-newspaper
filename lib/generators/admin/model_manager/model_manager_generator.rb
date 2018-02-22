2b100c0145fd70ba72762f4c6612db5ddb3cd832
Petar Nikolov <penikolov23@gmail.com>
2018-02-22 18:29:50 +0200

    Stop execution of generator if model does not exist

--- lib/generators/admin/model_manager/model_manager_generator.rb
+++ lib/generators/admin/model_manager/model_manager_generator.rb
@@ -4,11 +4,31 @@ class Admin::ModelManagerGenerator < Rails::Generators::NamedBase
 
   def initialize(*args, **kwargs)
     super(*args, **kwargs)
+
     @controller_name = file_name.singularize.capitalize
     @record_name = @controller_name.downcase
+
+    if !valid_input_data?
+      puts 'ERROR: the specified resource does not exist.',
+           'Please, ensure that a model with the given name exists.',
+           'NOTE: you may want to check if the passed name is namespaced correctly'
+    end
+  end
+
+  def valid_input_data?
+    begin
+      # Make sure a model whose name is passed as a command line argument exists
+      @controller_name.classify.constantize
+    rescue NameError
+      return false
+    end
+
+    true
   end
 
   def create_model_generator
+    return if !valid_input_data?
+
     model = @controller_name.classify.constantize
     @model_fields = model.attribute_names - ['id', 'created_at', 'updated_at']
     @model_fields.map! { |field| ":#{field}" }
@@ -18,6 +38,8 @@ class Admin::ModelManagerGenerator < Rails::Generators::NamedBase
   end
 
   def create_controller_views
+    return if !valid_input_data?
+
     restful_actions = ['index', 'show', 'new', 'edit']
     restful_actions.each do |action|
       template "#{action}.html.erb.erb",
@@ -26,6 +48,8 @@ class Admin::ModelManagerGenerator < Rails::Generators::NamedBase
   end
 
   def create_view_partials
+    return if !valid_input_data?
+
     @display_method = options['display-method']
     partials = { form: 'form', object: @record_name }
     partials.each do |template_name, partial|
--- test/lib/generators/admin/model_manager_generator_test.rb
+++ test/lib/generators/admin/model_manager_generator_test.rb
@@ -12,6 +12,19 @@ class Admin::ModelManagerGeneratorTest < Rails::Generators::TestCase
     end
   end
 
+  test 'generator does not create anything when specified model does not exist' do
+    run_generator ['some_model']
+    assert_no_file 'app/controllers/admin/users_controller.rb'
+
+    # Views
+    assert_no_file 'app/views/admin/users/index.html.erb'
+    assert_no_file 'app/views/admin/users/show.html.erb'
+    assert_no_file 'app/views/admin/users/new.html.erb'
+    assert_no_file 'app/views/admin/users/edit.html.erb'
+    assert_no_file 'app/views/admin/users/_form.html.erb'
+    assert_no_file 'app/views/admin/users/_user.html.erb'
+  end
+
   test 'generator creates a model controller' do
     run_generator ['user']
     assert_file 'app/controllers/admin/users_controller.rb'
