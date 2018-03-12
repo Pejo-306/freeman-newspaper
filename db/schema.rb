8cd184603476695131a9d7c378f0ddde2818dd6f
Petar Nikolov <penikolov23@gmail.com>
2018-03-12 23:51:41 +0200

    Generate articles model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180201154321) do
+ActiveRecord::Schema.define(version: 20180312215033) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
