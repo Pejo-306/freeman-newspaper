8730e8a58a7029ca0879f0622277cbefd8b8a09d
Petar Nikolov <penikolov23@gmail.com>
2018-03-13 01:12:52 +0200

    Add article show page

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -1,10 +1,20 @@
 require 'test_helper'
 
 class ArticlesControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @article = articles(:sample_article)
+  end
+
   test 'should get new' do
     get new_article_path
     assert_response :success
     assert_template 'articles/new'
+  end
+
+  test 'should get show' do
+    get article_path(@article)
+    assert_response :success
+    assert_template 'articles/show'
   end
 end
 
