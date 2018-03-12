ff046182ba932fad23506551e7c231140219c9f1
Petar Nikolov <penikolov23@gmail.com>
2018-03-13 00:05:21 +0200

    Add Article model validations
    Also, validations have been tested.

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -1,7 +1,28 @@
 require 'test_helper'
 
 class ArticleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @article = articles(:sample_article)
+  end
+
+  test 'should be valid' do
+    assert @article.valid?
+  end
+
+  test 'title should be present' do
+    @article.title = ''
+    assert @article.invalid?, 'Title is not present'
+  end
+
+  test 'title should be no longer than 100 characters long' do
+    @article.title = 'a' * 100
+    assert @article.valid?
+    @article.title += 'a'
+    assert @article.invalid?, 'Title is longer than 100 characters'
+  end
+
+  test 'content should be present' do
+    @article.content = ''
+    assert @article.invalid?, 'Content is not present'
+  end
 end
