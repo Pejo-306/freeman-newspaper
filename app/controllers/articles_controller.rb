d0cef52b537558933d7d4168c3c9100b968a6ea7
Petar Nikolov <penikolov23@gmail.com>
2018-04-14 23:29:57 +0300

    Implement article creation action
    Other notes:
      - provided titles on article pages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,5 +6,21 @@ class ArticlesController < ApplicationController
   def new
     @article = Article.new
   end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:success] = 'Article has been posted'
+      redirect_to articles_url
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
 end
 
