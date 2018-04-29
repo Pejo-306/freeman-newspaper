914deb9c396cd603da6869c284a7a1fac01e7ac3
Petar Nikolov <penikolov23@gmail.com>
2018-04-30 00:58:43 +0300

    Add author tests

---
+++ test/models/author_test.rb
@@ -0,0 +1,21 @@
+require 'test_helper'
+
+class AuthorTest < ActiveSupport::TestCase
+  setup do
+    @author = authors(:sample_author)
+  end
+
+  test 'should be valid' do
+    assert @author.valid?
+  end
+
+  test 'should have author status by default' do
+    new_author = Author.create(name: 'New',
+                               surname: 'Author',
+                               email: 'new@example.com',
+                               password: 'password',
+                               password_confirmation: 'password')
+    assert new_author.author?
+  end
+end
+
