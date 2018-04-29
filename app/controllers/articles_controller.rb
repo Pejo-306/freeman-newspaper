fca2ea56d509987facb6a7ef0dde66f4bfe39550
Petar Nikolov <penikolov23@gmail.com>
2018-04-30 00:44:29 +0300

    Permit only the author of an article to modify it
    Previously, any author could alter and/or delete any article they would
    like, even it was not written by said author. Now, only the author of an
    article can modify and/or delete it.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,6 +12,11 @@ class ArticlesController < ApplicationController
 
   def edit
     @article = Article.find(params[:id])
+    if @article.author != current_author
+      flash[:danger] = 'You do not have permission to alter this article ' +
+                       'because you are not its author'
+      redirect_to root_path
+    end
   end
 
   def create
@@ -19,7 +24,7 @@ class ArticlesController < ApplicationController
     @article.author = current_author
     if @article.save
       flash[:success] = 'Article has been posted'
-      redirect_to articles_url
+      redirect_to articles_path
     else
       render 'new'
     end
@@ -27,7 +32,11 @@ class ArticlesController < ApplicationController
 
   def update
     @article = Article.find(params[:id])
-    if @article.update_attributes(article_params)
+    if @article.author != current_author
+      flash[:danger] = 'You do not have permission to alter this article ' +
+                       'because you are not its author'
+      redirect_to root_path
+    elsif @article.update_attributes(article_params)
       flash[:success] = 'Article has successfully been updated'
       redirect_to article_path(@article)
     else
@@ -36,9 +45,16 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    Article.find(params[:id]).destroy
-    flash[:success] = 'Article has successfully been deleted'
-    redirect_to articles_path
+    article = Article.find(params[:id])
+    if article.author != current_author
+      flash[:danger] = 'You do not have permission to delete this article ' +
+                       'because you are not its author'
+      redirect_to root_path
+    else
+      article.destroy
+      flash[:success] = 'Article has successfully been deleted'
+      redirect_to articles_path
+    end
   end
 
   private
