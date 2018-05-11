98ff00a775855fc039bbc87eb19023445c4ed133
Petar Nikolov <penikolov23@gmail.com>
2018-05-12 00:53:58 +0300

    Associate columns to authors

--- test/models/column_test.rb
+++ test/models/column_test.rb
@@ -2,10 +2,17 @@ require 'test_helper'
 
 class ColumnTest < ActiveSupport::TestCase
   setup do
-    @sample_column = columns(:sample_column)
+    @column = columns(:sample_column)
   end
 
   test 'should be valid' do
-    assert @sample_column.valid?
+    assert @column.valid?, @column.errors.messages
+  end
+
+  test 'should belong to an author' do
+    assert_not_nil @column.author
+    assert @column.valid?
+    @column.author = nil
+    assert_not @column.valid?
   end
 end
