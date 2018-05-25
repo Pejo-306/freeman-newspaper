166298d8403c0f67e3b4e009cc38ac90c59d6ed6
Petar Nikolov <penikolov23@gmail.com>
2018-05-25 13:32:46 +0300

    Implement infinite scrolling to view a column's articles

--- app/controllers/columns_controller.rb
+++ app/controllers/columns_controller.rb
@@ -6,6 +6,12 @@ class ColumnsController < ApplicationController
     # Display a 404 error page for non-existent records
     raise ActiveRecord::RecordNotFound if @author.nil?
     @column = @author.column
+    @articles = @column.articles.paginate(page: params[:page], per_page: 10)
+                .order('updated_at DESC')
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 end
 
--- test/controllers/columns_controller_test.rb
+++ test/controllers/columns_controller_test.rb
@@ -10,5 +10,23 @@ class ColumnsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_template 'columns/show'
   end
+
+  test "should display the author's info" do
+    get column_path(@column.author.full_name)
+    assert_select 'h1', text: "#{@column.author.full_name}'s column"
+    assert_select 'p#author-biography', text: @column.author.biography
+  end
+
+  test "should display the column's articles" do
+    get column_path(@column.author.full_name)
+    assert_select 'ul#articles-list' do
+      assert_select 'li', count: @column.articles.count
+      @column.articles.each do |article|
+        assert_select 'a.article-link[href=?]', article_path(article)
+        assert_select 'h3', text: article.title
+        assert_select 'p.column-article-content', text: article.content
+      end
+    end
+  end
 end
 
