a48905f699ecf5d50e0184a66f87944d56249bea
Petar Nikolov <penikolov23@gmail.com>
2018-06-10 14:30:28 +0300

    Test topics index action

--- test/controllers/topics_controller_test.rb
+++ test/controllers/topics_controller_test.rb
@@ -7,6 +7,24 @@ class TopicsControllerTest < ActionDispatch::IntegrationTest
     @topic_exists = -> (name) { URI.encode "/topics/exists/#{name}" }
   end
 
+  test 'should get index' do
+    get topics_path
+    assert_response :success
+    assert_template 'topics/index'
+  end
+
+  test 'should paginate topics' do
+    get topics_path
+    assert_select 'main nav.pagination', count: 1
+    first_page_of_topics = Topic.paginate page: 1, per_page: 6
+    assert_select '#topics-list' do
+      assert_select '.topic-holder', 6
+    end
+    first_page_of_topics.each do |topic|
+      assert_select 'h3.topic-name', text: topic.name
+    end
+  end
+
   test 'should not get exists as an anonymous user' do
     get @topic_exists.call @topic.name 
     assert_response :redirect
