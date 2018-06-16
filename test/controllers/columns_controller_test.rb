34576891e8596c573b7780b16a6dc500442206aa
Petar Nikolov <penikolov23@gmail.com>
2018-06-16 18:44:08 +0300

    Include some small changes
    Changes:
      - add a few css ids to append articles only to the article list
        which contains all articles (bug fix)
      - minor changes to controller tests

--- test/controllers/columns_controller_test.rb
+++ test/controllers/columns_controller_test.rb
@@ -25,19 +25,17 @@ class ColumnsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should paginate the column's articles" do
-    travel 1.years do  # eliminate the presence of relevant articles
-      get column_path @column.author
-      assert_select 'main nav.pagination', count: 1
-      first_page_of_articles = @column.articles.paginate(page: 1, per_page: 10)
-      assert_select '.articles-list' do
-        assert_select 'li', 10
-      end
-      first_page_of_articles.each do |article|
-        assert_select 'a.article-link[href=?]', article_path(article)
-        assert_select 'h3', text: article.title
-        assert_select 'p', text: article.content
-        assert_select 'img[src=?]', article.thumbnail.url if article.thumbnail?
-      end
+    get column_path @column.author
+    assert_select 'main nav.pagination', count: 1
+    first_page_of_articles = @column.articles.paginate(page: 1, per_page: 10)
+    assert_select '#all-articles' do
+      assert_select 'li', 10
+    end
+    first_page_of_articles.each do |article|
+      assert_select 'a.article-link[href=?]', article_path(article)
+      assert_select 'h3', text: article.title
+      assert_select 'p', text: article.content
+      assert_select 'img[src=?]', article.thumbnail.url if article.thumbnail?
     end
   end
 end
