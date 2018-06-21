556cd04786b669503d2369043e59d937d5aeb07e
Petar Nikolov <penikolov23@gmail.com>
2018-06-21 17:50:17 +0300

    Display a number of the most relevant columns

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,7 @@ class StaticPagesController < ApplicationController
   def home
     @articles = most_relevant_articles Article.all, num: 8, max_days: 30
     @topics = Topic.order('RANDOM()').limit 7
+    @columns = most_relevant_columns Column.all, num: 7, max_days: nil
   end
 
   def about
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -7,12 +7,27 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_template 'home'
   end
 
+  test 'should display a number of articles' do
+    get root_path
+    assert_select '#all-articles' do
+      assert_select 'li', count: 8
+    end
+  end
+
   test 'should display some randomly selected topics' do
     get root_path
     assert_select '#topics-list' do
       assert_select '.topic-holder', count: 8
     end
     assert_select 'a[href=?]', topics_path, text: 'More topics'
+  end
+
+  test 'should display a number of columns' do
+    get root_path
+    assert_select '#columns-list' do
+      assert_select '.column-container', count: 8
+    end
+    assert_select 'a[href=?]', columns_path
   end
 
   test 'should get about' do
